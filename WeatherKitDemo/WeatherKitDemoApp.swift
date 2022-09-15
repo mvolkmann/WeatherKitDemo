750cd3fe5f12957401e4707989d649269cd1cab0
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-09-15 10:35:07 -0500

    Initial Commit

---
+++ WeatherKitDemo/WeatherKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherKitDemoApp.swift
+//  WeatherKitDemo
+//
+//  Created by Mark Volkmann on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

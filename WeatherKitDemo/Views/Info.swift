d3e620910d00cf1a09149336bed7221efc666ae2
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-02-08 11:07:40 -0600

    displaying AppIcon in Info.swift

--- WeatherKitDemo/Views/Info.swift
+++ WeatherKitDemo/Views/Info.swift
@@ -11,12 +11,10 @@ struct Info: View {
         VStack(spacing: 20) {
             if let appInfo {
                 Text(appInfo.name).font(.headline)
-                AsyncImage(
-                    url: URL(string: appInfo.iconURL),
-                    content: { image in image.resizable() },
-                    placeholder: { ProgressView() } // spinner
-                )
-                .frame(width: 100, height: 100)
+                Image("AppIcon") // doesn't work
+                Image(uiImage: UIImage(named: "AppIcon")!) // works
+                    .resizable()
+                    .frame(width: 100, height: 100)
                 Text("Created by R. Mark Volkmann")
             } else {
                 Text("Failed to access AppInfo.")

4e21444c9ebfe329f1d02ece7b9f71f20bc09a0d
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-03-03 13:23:52 -0600

    fixed DateExtension time computed property

--- WeatherKitDemo/Extensions/DateExtension.swift
+++ WeatherKitDemo/Extensions/DateExtension.swift
@@ -130,7 +130,7 @@ extension Date {
 
     var time: String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "h:m a"
+        dateFormatter.dateFormat = "h:mm a"
         return dateFormatter.string(from: self)
     }
 
